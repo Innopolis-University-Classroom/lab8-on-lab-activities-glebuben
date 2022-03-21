90371aef93bf7ca301fbc873506130ae8a3a33b6
glebuben <90417655+glebuben@users.noreply.github.com>
2022-03-21 15:16:14 +0300

    Update CalculatorWrong.java

--- src/com/muwaffaq/innopolis/solid/OCP_DI/wrong/CalculatorWrong.java
+++ src/com/muwaffaq/innopolis/solid/OCP_DI/wrong/CalculatorWrong.java
@@ -1,5 +1,3 @@
-package com.muwaffaq.innopolis.solid.OCP_DI.wrong;
-
 //❌❌❌❌
 // how many SOLID Principle does it violate ?
 // Fix it !!
@@ -9,37 +7,44 @@ package com.muwaffaq.innopolis.solid.OCP_DI.wrong;
 // DI : high = Abstract class
 // low : classes
 //
- class CalculatorWrong {
+class CalculatorWrong {
     private double left;
     private double right;
 
-    public enum Operation {ADD, SUBTRACT, DIVIDE,MULTIPLICATION}
+    public enum Operation {
+        ADD, SUBTRACT, DIVIDE, MULTIPLICATION;
+        double apply(double left, double right){
+            double result;
+            switch (this) {
+                case ADD:
+                    result = left + right;
+                    break;
+                case SUBTRACT:
+                    result = left - right;
+                    break;
+                case MULTIPLICATION:
+                    result = left * right;
+                    break;
+                default:
+                case DIVIDE:
+                    result = left / right;
+                    break;
+            }
+            return result;
+        }
+    }
 
     /// ❌ ❌ ❌ ❌
     /// this function violates the OP principle
     /// because it depend on concrete classes enum
     public  double calculate(Operation operation) {
-        double result;
-        switch (operation) {
-            case ADD:
-                result = left + right;
-                break;
-            case SUBTRACT:
-                result = left - right;
-                break;
-            default:
-            case DIVIDE:
-                result = left / right;
-                break;
-        }
-        return result;
+        return operation.apply(left, right);
     }
 
     public static void main(String[] args) {
         CalculatorWrong calculator = new CalculatorWrong();
         calculator.calculate(Operation.ADD);
         calculator.calculate(Operation.ADD);
-
     }
 
 }
